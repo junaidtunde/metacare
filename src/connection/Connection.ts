b645f60aa0ff53d5f5944db6d421d3e57f5ffea8
junaid <junaidtunde1@gmail.com>
2021-09-24 00:44:16 +0100

    fix: issue with heroku

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -6,17 +6,15 @@ require('dotenv').config();
 
 export const connection = createConnection({
     type: 'postgres', 
-    // host: process.env.PGHOST,
-    // port:  Number(process.env.PGPORT), // default port of postgres
-    // username: process.env.PGUSER, // our created username, you can have your own user name
-    // password: process.env.PGPASSWORD, // our created username, you can have your own password
-    // database: process.env.PGDATABASE, // our created database name, you can have your own
-    url: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    port:  Number(process.env.PGPORT), // default port of postgres
+    username: process.env.PGUSER, // our created username, you can have your own user name
+    password: process.env.PGPASSWORD, // our created username, you can have your own password
+    database: process.env.PGDATABASE, // our created database name, you can have your own
     entities: [
         // typeORM will not be able to create database table if we forget to put entity class name here..
         Comments
     ],
     synchronize: true,
-    logging: false,
-    ssl: true
+    logging: false
 });
