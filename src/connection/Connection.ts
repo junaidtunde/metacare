268313904fa30ff4de64a44a5e45b1927266ecf4
junaid <junaidtunde1@gmail.com>
2021-09-24 01:06:23 +0100

    feat: heroku dyno

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -4,6 +4,23 @@ import Comments from "../entity/Comments";
 
 require('dotenv').config();
 
+console.log(
+    {
+    type: 'postgres', 
+    host: process.env.PGHOST,
+    port:  Number(process.env.PGPORT), // default port of postgres
+    username: process.env.PGUSER, // our created username, you can have your own user name
+    password: process.env.PGPASSWORD, // our created username, you can have your own password
+    database: process.env.PGDATABASE, // our created database name, you can have your own
+    entities: [
+        // typeORM will not be able to create database table if we forget to put entity class name here..
+        Comments
+    ],
+    synchronize: true,
+    logging: false
+}
+)
+
 export const connection = createConnection({
     type: 'postgres', 
     host: process.env.PGHOST,
